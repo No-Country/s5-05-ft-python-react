0ffb0f9171238b29f4ec7d3c888d80b28da943ef
imjesusleal <jesusalbertolv@gmail.com>
2022-11-14 23:16:24 -0300

    Serializador de jugador funcionando

--- Back/main/serializer.py
+++ Back/main/serializer.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 from .models import User, Jugador
 
-class JugadorSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model= User
         fields = "__all__"
 
+class JugadorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Jugador
+        fields = "__all__"
+
+    
     def create(self):
         user = User.objects.create()
         Jugador.objects.create(user=user)
