1b158725a9413c3f5d1cef10878068c00a656a33
Smael Nicolas <nico.smael@gmail.com>
2022-11-16 18:48:54 -0300

    add big container with background , add position in court

--- Front/src/pages/ProfilePlayer/ProfilePlayer.jsx
+++ Front/src/pages/ProfilePlayer/ProfilePlayer.jsx
@@ -3,71 +3,94 @@ import AvailableIcon from "../../assets/profile/available_icon.png";
 import GenreIcon from "../../assets/profile/genre_icon.png";
 import LevelIcon from "../../assets/profile/level_icon.png";
 import MalePlayer from "../../assets/profile/male_player.png";
+import PositionIcon from "../../assets/profile/position_icon.png";
 import WtsIcon from "../../assets/profile/wts_icon.png";
+import { openInNewTab } from "../../helpers/openInNewTab";
 import "./profilePlayer.css";
 
 export const ProfilePlayer = () => {
 	return (
-		<div className='container--profilePlayer'>
-			<div className='container--profilePlayer--avatar--name'>
-				<img
-					className='profilePlayer--avatar'
-					src={MalePlayer}
-					alt='avatar icon'
-				/>
-				{/* <img
+		<div className='container--section--profilePlayer'>
+			<div className='container--profilePlayer'>
+				<div className='container--profilePlayer--avatar--name'>
+					<img
+						className='profilePlayer--avatar'
+						src={MalePlayer}
+						alt='avatar icon'
+					/>
+					{/* <img
 					className='profilePlayer--avatar'
 					src={FemalePlayer}
 					alt='avatar icon'
 				/> */}
-				<div className='container--profilePlayer--name'>
-					<div className='profilePlayer--lastName'>
-						apellido{" "}
-						<span className='profilePlayer--lastName--span'>,</span>
-					</div>
-					<div className='profilePlayer--name'>nombre</div>
-				</div>
-			</div>
-			<div className='container--profilePlayer--info'>
-				<div className='container--profilePlayer--info--level'>
-					<img
-						className='profilePlayer--info--icon'
-						src={LevelIcon}
-						alt='player level'
-					/>
-					<div className='profilePlayer--info--level--text'>
-						Nivel de Juego
-					</div>
-					<div className='profilePlayer--info--level--number'>7</div>
-				</div>
-				<div className='container--profilePlayer--info--available'>
-					<img
-						className='profilePlayer--info--icon'
-						src={AvailableIcon}
-						alt='player hours'
-					/>
-					<div className='profilePlayer--info--available--text'>
-						Horarios Disponibles
+					<div className='container--profilePlayer--name'>
+						<div className='profilePlayer--lastName'>
+							apellido{" "}
+							<span className='profilePlayer--lastName--span'>
+								,
+							</span>
+						</div>
+						<div className='profilePlayer--name'>nombre</div>
 					</div>
 				</div>
-				<div className='container--profilePlayer--info--contact'>
-					<img
-						className='profilePlayer--info--icon'
-						src={WtsIcon}
-						alt='player contact'
-					/>
-					<div className='profilePlayer--info--contact--number'>
-						126 - 46456463
+				<div className='container--profilePlayer--info'>
+					<div className='container--profilePlayer--info--level'>
+						<img
+							className='profilePlayer--info--icon'
+							src={LevelIcon}
+							alt='player level'
+						/>
+						<div className='profilePlayer--info--level--text'>
+							Nivel
+						</div>
+						<div className='profilePlayer--info--level--number'>
+							7
+						</div>
 					</div>
-				</div>
-				<div className='container--profilePlayer--info--genre'>
-					<img
-						className='profilePlayer--info--icon'
-						src={GenreIcon}
-						alt='player genre'
-					/>
-					<div className='profilePlayer--info--genre--text'>
-						Hombre
+					<div className='container--profilePlayer--info--position'>
+						<img
+							className='profilePlayer--info--icon'
+							src={PositionIcon}
+							alt='player level'
+						/>
+						<div className='profilePlayer--position--value'>
+							Drive
+						</div>
+					</div>
+					<div className='container--profilePlayer--info--available'>
+						<img
+							className='profilePlayer--info--icon'
+							src={AvailableIcon}
+							alt='player hours'
+						/>
+						<div className='profilePlayer--info--available--text'>
+							Horarios Disponibles
+						</div>
+					</div>
+					<div className='container--profilePlayer--info--contact'>
+						<img
+							className='profilePlayer--info--icon'
+							src={WtsIcon}
+							alt='player contact'
+						/>
+
+						<div
+							onClick={() =>
+								openInNewTab(`https://wa.me/123456789`)
+							}
+							className='profilePlayer--info--contact--number'>
+							126 - 46456463
+						</div>
+					</div>
+					<div className='container--profilePlayer--info--genre'>
+						<img
+							className='profilePlayer--info--icon'
+							src={GenreIcon}
+							alt='player genre'
+						/>
+						<div className='profilePlayer--info--genre--text'>
+							Hombre
+						</div>
 					</div>
 				</div>
 			</div>
