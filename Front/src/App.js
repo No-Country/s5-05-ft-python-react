83a43d1bcd313ee5fa8b144230a014f4c8c2d5bd
Smael Nicolas <nico.smael@gmail.com>
2022-11-16 19:57:44 -0300

    fix element showin on route profile

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { HomeCourt } from "./pages/HomeCourt";
+import { Profile } from "./pages/Profile/Profile";
 import { ProfileComplex } from "./pages/ProfileComplex/ProfileComplex";
 import { ProfilePlayer } from "./pages/ProfilePlayer/ProfilePlayer";
 
@@ -16,7 +17,7 @@ export const App = () => {
 				path='/profile/player/:idPlayer'
 				element={<ProfilePlayer />}
 			/>
-			<Route path='/profile' element={<ProfilePlayer />} />
+			<Route path='/profile' element={<Profile />} />
 			<Route path='/login' />
 			<Route path='/register' />
 		</Routes>
