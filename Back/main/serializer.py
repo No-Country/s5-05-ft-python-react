93ca2485919551ab9528124e655b0a126aeb5723
imjesusleal <jesusalbertolv@gmail.com>
2022-11-30 19:25:11 -0300

    Auth final y CORS

--- Back/main/serializer.py
+++ Back/main/serializer.py
@@ -40,10 +40,6 @@ class UserTokenSerializer(serializers.ModelSerializer):
         if username and password:
             user = authenticate(request=self.context.get('request'),
                                 username=username, password=password)
-
-            # The authenticate call simply returns None for is_active=False
-            # users. (Assuming the default ModelBackend authentication
-            # backend.)
             if not user:
                 msg = ('Unable to log in with provided credentials.')
                 raise serializers.ValidationError(msg, code='authorization')
