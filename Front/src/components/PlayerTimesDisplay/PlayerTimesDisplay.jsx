fd53b4c4ebf9396015e07825b581c4abd4db05f4
Smael Nicolas <nico.smael@gmail.com>
2022-11-29 23:44:30 -0300

    render times for each player

--- Front/src/components/PlayerTimesDisplay/PlayerTimesDisplay.jsx
+++ Front/src/components/PlayerTimesDisplay/PlayerTimesDisplay.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { findDay, findHour } from "../../helper/timePlayers";
+import { Loading } from "../Loading/Loading";
+
+import styles from "./playerTimesDisplay.module.css";
+
+const { container, labelDay, containerDays, containerTimes, timeText } = styles;
 
 const days = {
 	lun: [],
@@ -26,21 +31,66 @@ export const PlayerTimesDisplay = ({ availableTimes }) => {
 				});
 			}
 		});
+
 		setTimeout(() => {
 			setLoading(false);
 		}, 3000);
 	}, []);
 
-	const log = (value) => {
-		console.log(value);
+	const renderDays = (day) => {
+		return day.map((dateTime) => (
+			<div key={dateTime} className={timeText}>
+				{dateTime}
+			</div>
+		));
 	};
 
 	return loading ? (
-		<>Cargando ...</>
+		<Loading />
 	) : (
-		<div>
-			PlayerTimesDisplay
-			{log(days)}
+		<div className={container}>
+			{days.dom.length > 0 && (
+				<div className={containerDays}>
+					<div className={labelDay}>Domingo</div>
+					<div className={containerTimes}>{renderDays(days.dom)}</div>
+				</div>
+			)}
+			{days.lun.length > 0 && (
+				<div className={containerDays}>
+					<div className={labelDay}>Lunes</div>
+					<div className={containerTimes}>{renderDays(days.lun)}</div>
+				</div>
+			)}
+			{days.mar.length > 0 && (
+				<div className={containerDays}>
+					<div className={labelDay}>Martes</div>
+					<div className={containerTimes}>{renderDays(days.mar)}</div>
+				</div>
+			)}
+			{days.mie.length > 0 && (
+				<div className={containerDays}>
+					<div className={labelDay}>Miercoles</div>
+					<div className={containerTimes}>{renderDays(days.mie)}</div>
+				</div>
+			)}
+			{days.jue.length > 0 && (
+				<div className={containerDays}>
+					<div className={labelDay}>Jueves</div>
+					<div className={containerTimes}>{renderDays(days.jue)}</div>
+				</div>
+			)}
+			{days.vie.length > 0 && (
+				<div className={containerDays}>
+					<div className={labelDay}>Viernes</div>
+					<div className={containerTimes}>{renderDays(days.vie)}</div>
+				</div>
+			)}
+			{days.sab.length > 0 && (
+				<div className={containerDays}>
+					<div className={labelDay}>Sabado</div>
+					<div className={containerTimes}>{renderDays(days.sab)}</div>
+				</div>
+			)}
 		</div>
 	);
 };
