6e5bcdd823d5a641373ea6c7a52ecd8dd26904b3
Smael Nicolas <nico.smael@gmail.com>
2022-12-05 19:38:17 -0300

    add only number validation

--- Front/src/helper/validations.js
+++ Front/src/helper/validations.js
@@ -13,3 +13,7 @@ export const validStringNumber = (string) => {
 	if (string.trim().length < 9) return false;
 	return /^[0-9]+$/.test(string);
 };
+
+export const validNumberString = (string) => {
+	return /^\d+$/.test(string);
+};
