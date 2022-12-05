5098d18bd1cf039c26d09ee0a64f72f9cd23b1c0
Smael Nicolas <nico.smael@gmail.com>
2022-12-05 14:44:10 -0300

    dele api calls ex in app

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,8 +1,6 @@
-import { json, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import { PlayerTimesDisplay } from "./components/PlayerTimesDisplay/PlayerTimesDisplay";
-import { SelectTime2 } from "./components/selectTime/SelectTime";
 import { Complex } from "./pages/Complex/Complex";
 import { ComplexForm } from "./pages/ComplexForm/ComplexForm";
 import { Homepage } from "./pages/HomePage/HomePage";
@@ -15,30 +13,6 @@ import { ProfilePlayer } from "./pages/ProfilePlayer/ProfilePlayer";
 import { Register } from "./pages/Register/Register";
 
 export const App = () => {
-	const user = {
-		email: "nico7@gmail.com",
-		password: "12345",
-		is_jugador: true,
-		is_complejo: false,
-		perfil_completo: false,
-	};
-	// post
-	fetch("http://127.0.0.1:8000/api/usuario/", {
-		method: "POST",
-		body: JSON.stringify(user),
-		mode: "cors", // no-cors, *cors, same-origin
-		cache: "no-cache",
-		credentials: "same-origin", // include, *same-origin, omit
-		headers: {
-			"Content-Type": "application/json",
-		},
-	});
-
-	// get
-	fetch("http://127.0.0.1:8000/api/usuario/")
-		.then((response) => response.json())
-		.then((rta) => console.log(rta));
-
 	return (
 		<Routes>
 			<Route path='/' element={<Homepage />} />
