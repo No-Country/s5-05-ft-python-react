ffad7a5b526160f8e66dfd57b9d19cabbceb695d
Gabriel Chazarreta <gfchazarreta@gmail.com>
2022-12-05 23:44:40 -0300

    Adding data in Complex page

--- Front/src/components/ComplexList/Card/Card.jsx
+++ Front/src/components/ComplexList/Card/Card.jsx
@@ -6,23 +6,23 @@ import classes from './Card.module.css';
 
 const { card, img, text__container, text, btn } = classes;
 
-export const Card = ({imgUrl}) => {
+export const Card = ({complex}) => {
 
 const [openModal, setOpenModal] = useState(false)
 
   return (
     <div className={card}>
       {
-        openModal && <Modal children={<ModalCard setOpenModal={setOpenModal} imgUrl={imgUrl}/>}/>
+        openModal && <Modal children={<ModalCard setOpenModal={setOpenModal} imgUrl="https://i.pinimg.com/originals/e7/e0/c3/e7e0c3f67fae37d36c0e436c67488689.jpg" complex={complex}/>}/>
       }
-        <img src={imgUrl} alt="complejo" className={img}/>
+        <img src="https://i.pinimg.com/originals/e7/e0/c3/e7e0c3f67fae37d36c0e436c67488689.jpg" alt="complejo" className={img}/>
         <div className={text__container}>
           <div className={text}>
-              <h2>Complejo 1</h2>
-              <p>Cantidad de canchas: 3</p>
-              <p>Horarios: 8AM - 23PM</p>
+              <h2>{complex?.nombre}</h2>
+              <p>Cantidad de canchas: {complex?.cant_cancha}</p>
+              <p>Dirección: {complex.calle} {complex.altura}</p>
           </div>
-          <button className={btn} onClick={() => setOpenModal(true)}>Inscribirse</button>
+          <button className={btn} onClick={() => setOpenModal(true)}>Información</button>
         </div>
     </div>
   )
--- Front/src/components/ComplexList/ComplexList.jsx
+++ Front/src/components/ComplexList/ComplexList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { Card } from './Card/Card';
 
 import classes from './ComplexList.module.css';
@@ -6,18 +6,22 @@ import classes from './ComplexList.module.css';
 const { container, card__container } = classes;
 
 export const ComplexList = () => {
+
+  const [complexRenderList, setComplexRenderList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/complejo').then(res=>res.json()).then(data=>setComplexRenderList(data))
+  }, [])
+  
+
+  console.log(complexRenderList);
+
   return (
     <div className={container}>
         <div className={card__container}>
-            <Card imgUrl="https://infodeportes.com.ar/wp-content/uploads/2022/01/Stylo-padel-club-abre-sus-puertas-en-ciudad-Perico.png"/>
-            <Card imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrSBV8iIUOx6gsqmhgMJ17FKDbPazCvaa-nw&usqp=CAU"/>
-            <Card imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh6lyF2txDSccaT8B1_7cetenghQp4cWCw3A&usqp=CAU"/>
-            <Card imgUrl="https://infodeportes.com.ar/wp-content/uploads/2022/01/Stylo-padel-club-abre-sus-puertas-en-ciudad-Perico.png"/>
-            <Card imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrSBV8iIUOx6gsqmhgMJ17FKDbPazCvaa-nw&usqp=CAU"/>
-            <Card imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh6lyF2txDSccaT8B1_7cetenghQp4cWCw3A&usqp=CAU"/>
-            <Card imgUrl="https://infodeportes.com.ar/wp-content/uploads/2022/01/Stylo-padel-club-abre-sus-puertas-en-ciudad-Perico.png"/>
-            <Card imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrSBV8iIUOx6gsqmhgMJ17FKDbPazCvaa-nw&usqp=CAU"/>
-            <Card imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh6lyF2txDSccaT8B1_7cetenghQp4cWCw3A&usqp=CAU"/>
+            {
+              complexRenderList?.map(complex=><Card complex={complex} key={complex.usuario}/>)
+            }
         </div>
       
     </div>
