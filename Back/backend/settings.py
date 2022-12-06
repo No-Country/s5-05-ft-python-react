def4eb831d334873e3c47c0c5331cac63902da43
imjesusleal <jesusalbertolv@gmail.com>
2022-12-06 17:27:26 -0300

    Statics fixpatch

--- Back/backend/settings.py
+++ Back/backend/settings.py
@@ -136,10 +136,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = './Back/static/'
+STATIC_URL = 'static/'
 
 if not DEBUG:   
-    STATIC_ROOT = os.path.join(BASE_DIR, './Back/staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
