d5d49bfade68d6d9894579857a38dd387d7af0d7
Gabriel Chazarreta <gfchazarreta@gmail.com>
2022-12-07 18:57:17 -0300

    Adding axios to get data in Players & Complex

--- Front/src/components/ComplexList/Card/Card.jsx
+++ Front/src/components/ComplexList/Card/Card.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom';
 import { Modal } from '../../Modal/Modal';
 import { ModalCard } from '../ModalCard/ModalCard';
 
@@ -12,9 +13,9 @@ const [openModal, setOpenModal] = useState(false)
 
   return (
     <div className={card}>
-      {
+      {/* {
         openModal && <Modal children={<ModalCard setOpenModal={setOpenModal} imgUrl="https://i.pinimg.com/originals/e7/e0/c3/e7e0c3f67fae37d36c0e436c67488689.jpg" complex={complex}/>}/>
-      }
+      } */}
         <img src="https://i.pinimg.com/originals/e7/e0/c3/e7e0c3f67fae37d36c0e436c67488689.jpg" alt="complejo" className={img}/>
         <div className={text__container}>
           <div className={text}>
@@ -22,7 +23,9 @@ const [openModal, setOpenModal] = useState(false)
               <p>Cantidad de canchas: {complex?.cant_cancha}</p>
               <p>Dirección: {complex.calle} {complex.altura}</p>
           </div>
-          <button className={btn} onClick={() => setOpenModal(true)}>Información</button>
+          <Link to={`/profile/complex/${complex.usuario}`}>
+            <button className={btn}>Información</button>
+          </Link>
         </div>
     </div>
   )
