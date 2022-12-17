4876fb74e902264dbab90998bfa017d2581c4b3a
Santiago Muñoz <san88.zzl.308@gmail.com>
2022-12-17 18:29:13 -0300

    probando deploy v1.0

--- Back/main/models.py
+++ Back/main/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from multiselectfield import MultiSelectField
 
 from django.contrib.postgres.fields import ArrayField
 from datetime import datetime
